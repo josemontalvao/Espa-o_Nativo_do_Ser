2deb58ed7e14459beba7eeab6d5c8d51da4a22d7
José Antônio Da Conceição Montalvão <jokmontalvao@gmail.com>
2021-10-23 13:19:20 -0300

    menu hamburger

--- JS/menu.js
+++ JS/menu.js
@@ -1,10 +1,10 @@
 let show = true;
 
-const menuSection = document.querySelector(".menu-section")
-const menuToggle = menuSection.querySelector(".nemu-toggle")
+const menuSection = document.querySelector(".menu-section");
+const menuToggle = menuSection.querySelector(".menu-toggle");
 
 menuToggle.addEventListener("click", () => {
-document.body.style.overflow = show ? "hidden" : "initial"    
-menuSection.classList.toggle("on", show)
-show = !show;
-})
+    document.body.style.overflow = show ? "hidden" : "initial";
+    menuSection.classList.toggle("on", show);
+    show = !show;
+});
